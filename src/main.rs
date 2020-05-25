e5e86a61a5833cf5a57304d9f8bc8c65eeb0fe39
Justin Auger <justinauger@gmail.com>
2020-05-25 12:05:52 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-#[macro_use] extern crate log;
-use std::collections::VecDeque;
-use tokio;
-use futures::future::FutureExt;
+#[macro_use]
+extern crate log;
 use bson;
 use bson::doc;
-use serde_json::json;
+use futures::future::FutureExt;
 use serde_json;
+use serde_json::json;
+use std::collections::VecDeque;
 use std::sync::Arc;
+use tokio;
 
-use riven::{RiotApi, RiotApiConfig};
+use mongodb::options::FindOptions;
+use mongodb::{options::ClientOptions, Client};
 use riven::consts::Region;
 use riven::models::tft_league_v1::LeagueList;
-use mongodb::{Client, options::ClientOptions};
-use mongodb::options::FindOptions;
+use riven::{RiotApi, RiotApiConfig};
 
 use promise_buffer::promise_buffer;
 
@@ -21,21 +22,23 @@ mod promise_buffer;
 #[tokio::main]
 async fn main() -> () {
     env_logger::init();
-    
+
     let api = {
         let api_key = std::env::var("RGAPI_KEY").expect("Missing environment variable: RGAPI_KEY");
         let api_config = RiotApiConfig::with_key(api_key).preconfig_throughput();
         Arc::new(RiotApi::with_config(api_config))
     };
-    
+
     let db = {
-        let db_connection_string = std::env::var("DB_CONNECTION_STRING").expect("Missing environment variable: DB_CONNECTION_STRING");
-        let mut client_options = ClientOptions::parse(&db_connection_string).expect("Unable to parse DB options");
+        let db_connection_string = std::env::var("DB_CONNECTION_STRING")
+            .expect("Missing environment variable: DB_CONNECTION_STRING");
+        let mut client_options =
+            ClientOptions::parse(&db_connection_string).expect("Unable to parse DB options");
         client_options.app_name = Some("tft_stat".to_string());
         let client = Client::with_options(client_options).expect("Unable to construct DB client");
         Arc::new(client.database("tft"))
     };
-    
+
     let m1 = Main {
         region: Region::NA,
         region_major: Region::AMERICAS,
@@ -75,29 +78,36 @@ async fn main() -> () {
 }
 
 struct Main {
-    api: Arc<RiotApi>,    
+    api: Arc<RiotApi>,
     region: Region,
     region_major: Region,
-    db: Arc<mongodb::Database>
+    db: Arc<mongodb::Database>,
 }
 
 impl Main {
     async fn do_cycle(&self) {
         info!("[{}] Main begin.", self.region);
         let summoner_list = self.get_top_players().await;
-        info!("[{}] Gathered summoner ids for {} players.", self.region, summoner_list.len());
+        info!(
+            "[{}] Gathered summoner ids for {} players.",
+            self.region,
+            summoner_list.len()
+        );
 
         // VecDeque of Futures
-        let q: VecDeque<_> = summoner_list.iter().enumerate().map(|(index, id)| self.process_summoner_id(index, id).boxed()).collect();
-        
-        promise_buffer(q, 1, |result| {
-            match result {
-                Ok(_) => {}
-                Err(e) => {
-                    error!("{:#?}", e);
-                },
+        let q: VecDeque<_> = summoner_list
+            .iter()
+            .enumerate()
+            .map(|(index, id)| self.process_summoner_id(index, id).boxed())
+            .collect();
+
+        promise_buffer(q, 1, |result| match result {
+            Ok(_) => {}
+            Err(e) => {
+                error!("{:#?}", e);
             }
-        }).await;
+        })
+        .await;
 
         info!("[{}] Main Done.", self.region);
     }
@@ -105,26 +115,43 @@ impl Main {
     /// Do all processing for a single summoner
     /// Propagates up errors from database and api calls (but not match fetching errors)
     async fn process_summoner_id(&self, index: usize, id: &str) -> anyhow::Result<()> {
-        let player = self.api.tft_summoner_v1().get_by_summoner_id(self.region, id).await?;
-        let player_match = self.api.tft_match_v1().get_match_ids_by_puuid(self.region_major, &player.puuid, Some(10)).await?;
+        let player = self
+            .api
+            .tft_summoner_v1()
+            .get_by_summoner_id(self.region, id)
+            .await?;
+        let player_match = self
+            .api
+            .tft_match_v1()
+            .get_match_ids_by_puuid(self.region_major, &player.puuid, Some(10))
+            .await?;
 
         let mut new: i32 = 0;
         let mut repeat: i32 = 0;
         let mut new_error: i32 = 0;
         for x in &player_match {
             match self.process_match_id(&x).await {
-                Err(e) =>  error!("{:#?}", e),
+                Err(e) => error!("{:#?}", e),
                 Ok(-1) => new_error += 1,
                 Ok(0) => repeat += 1,
                 Ok(1) => new += 1,
                 Ok(_) => unreachable!(),
             }
         }
-        debug!("{} {} {:#?} {} ({} New, {} Old, {} Error)", index, self.region, player.name, player_match.len(), new, repeat, new_error);
+        debug!(
+            "{} {} {:#?} {} ({} New, {} Old, {} Error)",
+            index,
+            self.region,
+            player.name,
+            player_match.len(),
+            new,
+            repeat,
+            new_error
+        );
         Ok(())
     }
 
-    async fn process_match_id(&self, id: &str) -> anyhow::Result<i64>{
+    async fn process_match_id(&self, id: &str) -> anyhow::Result<i64> {
         let matches = self.db.collection("matches_v2");
         let filter = doc! {"_id": id};
         let find_options = FindOptions::default();
@@ -137,7 +164,12 @@ impl Main {
             return Ok(0);
         }
 
-        let game = match self.api.tft_match_v1().get_match(self.region_major, id).await {
+        let game = match self
+            .api
+            .tft_match_v1()
+            .get_match(self.region_major, id)
+            .await
+        {
             Ok(g) => g,
             Err(e) => {
                 // let req_err = e.source_reqwest_error().to_string();
@@ -148,12 +180,14 @@ impl Main {
 
         let mut game_json = match &game {
             None => json!({}),
-            Some(g) =>  serde_json::to_value(g)?
+            Some(g) => serde_json::to_value(g)?,
         };
         game_json["_id"] = json!(id);
         let bson: bson::Bson = game_json.into();
 
-        let doc = bson.as_document().ok_or_else(|| anyhow::Error::msg("Error creating mongo doc"))?;
+        let doc = bson
+            .as_document()
+            .ok_or_else(|| anyhow::Error::msg("Error creating mongo doc"))?;
         matches.insert_one(doc.clone(), None)?;
 
         match &game {
@@ -164,7 +198,7 @@ impl Main {
 
     async fn get_top_players(&self) -> Vec<String> {
         let mut ret = Vec::new();
-    
+
         for (tier, division) in &[
             ("CHALLENGER", "I"),
             ("GRANDMASTER", "I"),
@@ -193,13 +227,28 @@ impl Main {
         }
         ret
     }
-    
+
     async fn get_league_entries(&self, tier: &str, division: &str) -> anyhow::Result<Vec<String>> {
         // non-paginated cases
         let x: Option<LeagueList> = match tier {
-            "CHALLENGER" => Some(self.api.tft_league_v1().get_challenger_league(self.region).await?),
-            "GRANDMASTER" => Some(self.api.tft_league_v1().get_grandmaster_league(self.region).await?),
-            "MASTER" => Some(self.api.tft_league_v1().get_master_league(self.region).await?),
+            "CHALLENGER" => Some(
+                self.api
+                    .tft_league_v1()
+                    .get_challenger_league(self.region)
+                    .await?,
+            ),
+            "GRANDMASTER" => Some(
+                self.api
+                    .tft_league_v1()
+                    .get_grandmaster_league(self.region)
+                    .await?,
+            ),
+            "MASTER" => Some(
+                self.api
+                    .tft_league_v1()
+                    .get_master_league(self.region)
+                    .await?,
+            ),
             _ => None,
         };
         if let Some(ll) = x {
@@ -211,16 +260,20 @@ impl Main {
         let mut page = 1;
         let mut ret = Vec::new();
         loop {
-            let x = self.api.tft_league_v1().get_league_entries(self.region, tier, division, Some(page)).await?;
-            if x.len() == 0 { break };
-    
+            let x = self
+                .api
+                .tft_league_v1()
+                .get_league_entries(self.region, tier, division, Some(page))
+                .await?;
+            if x.len() == 0 {
+                break;
+            };
+
             for y in x {
                 ret.push(y.summoner_id.clone());
             }
-            page+=1;
+            page += 1;
         }
         Ok(ret)
     }
 }
-
-
--- src/promise_buffer.rs
+++ src/promise_buffer.rs
@@ -1,20 +1,23 @@
-
-use core::pin::Pin;
 use core::future::Future;
+use core::pin::Pin;
 
 use std::collections::VecDeque;
 
 /// Generic to take a list of promises and execute all with a specific level of parrallelism
-pub async fn promise_buffer<'a,T,F>(mut q: VecDeque<Pin<Box<dyn Future<Output = T> + std::marker::Send + 'a>>>, sz: usize, mut on_result: F)
--> () 
-    where F: FnMut(T) -> ()
-{    
+pub async fn promise_buffer<'a, T, F>(
+    mut q: VecDeque<Pin<Box<dyn Future<Output = T> + std::marker::Send + 'a>>>,
+    sz: usize,
+    mut on_result: F,
+) -> ()
+where
+    F: FnMut(T) -> (),
+{
     let mut vec: Vec<_> = Vec::new();
     loop {
         while !(vec.len() == sz) {
             match q.pop_front() {
                 Some(x) => vec.push(x),
-                None => break
+                None => break,
             }
         }
         if vec.len() == 0 {
@@ -23,7 +26,6 @@ pub async fn promise_buffer<'a,T,F>(mut q: VecDeque<Pin<Box<dyn Future<Output = 
         let (result, _index, z) = futures::future::select_all(vec).await;
         on_result(result);
         vec = z;
-        // info!("{:#?}{:#?}{:#?}", y, vec.len() );        
+        // info!("{:#?}{:#?}{:#?}", y, vec.len() );
     }
-
 }
